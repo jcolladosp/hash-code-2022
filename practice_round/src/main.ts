1e58a886aec1b57cf070fd28e7765584bda99d01
jsan <jsan@edicom.es>
2022-02-23 17:02:52 +0100

    [FEAT] ~minor :sparkles: Typescript experimental y modulos nativos de ECMAScript [Link a la tarea ](https://apps.edicomgroup.com/JDev/#idstask|) /label ~"status::needs triage" ~type::feat ~minor /assign me

--- practice_round/src/main.ts
+++ practice_round/src/main.ts
@@ -1,5 +1,5 @@
 import path from 'path';
-import { readDataset } from '../utils/dataset-reader';
+import { readDataset } from '../utils/dataset-reader.js';
 
 readDataset(path.resolve(process.cwd(), './practice_round/datasets/a_an_example.in.txt')).then(
   (dataset) => console.log(dataset),
